47d3727ab2989b78d5075a29d882744b3ea1b560
Charlie Davis <34718612+charliecarrot@users.noreply.github.com>
2024-02-18 18:43:44 -0700

    Update website background styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,16 +61,43 @@ const Home: NextPage = () => {
                 </div>
             </Box>
 
-            <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-                <Image
-                    className='relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert'
-                    src='/next.svg'
-                    alt='Next.js Logo'
-                    width={180}
-                    height={37}
-                    priority
-                />
-            </div>
+            <Box
+                sx={{
+                    position: 'relative',
+                    display: 'flex',
+                    alignItems: 'center',
+                    zIndex: -1,
+                    transition: '1s',
+                    left: '20%',
+                    '&:before': {
+                        content: '""',
+                        position: 'absolute',
+                        height: { xs: '300px', sm: '320px', md: '340px', lg: '360px' },
+                        width: { xs: '100vw', sm: '480px', md: '550px', lg: '680px' },
+                        left: '50%',
+                        transform: 'translateX(-50%)',
+                        borderRadius: '50%',
+                        background: 'radial-gradient(circle, #54126350, #3B126390)',
+                        opacity: 0.1,
+                        filter: 'blur(40px)',
+                        zIndex: -1,
+                        transition: '1s',
+                    },
+                    '&:after': {
+                        content: '""',
+                        position: 'absolute',
+                        zIndex: -20,
+                        height: { xs: '180px', sm: '180px', md: '190px', lg: '200px' },
+                        width: { xs: '100px', sm: '240px', md: '290px', lg: '360px' },
+                        right: '33%',
+                        transform: 'translateX(33%)',
+                        background: 'conic-gradient(from 0deg, #5B1263, #2F1263, #3B1263)',
+                        opacity: 0.4,
+                        filter: 'blur(40px)',
+                        transition: '1s',
+                    },
+                }}
+            ></Box>
 
             <div className='mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left'>
                 <a
