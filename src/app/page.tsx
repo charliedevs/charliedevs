ecab4fb12ea1baefbc0b0efbf659ba21aa1606b8
Charlie Davis <34718612+charliecarrot@users.noreply.github.com>
2024-02-18 22:53:52 -0700

    Add image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { Box, Typography } from '@mui/material'
 import { grey } from '@mui/material/colors'
 import type { NextPage } from 'next'
 import Image from 'next/image'
+import SelfImage from '@/components/Images/SelfImage'
 
 const Home: NextPage = () => {
     return (
@@ -61,43 +62,7 @@ const Home: NextPage = () => {
                 </div>
             </Box>
 
-            <Box
-                sx={{
-                    position: 'relative',
-                    display: 'flex',
-                    alignItems: 'center',
-                    zIndex: -1,
-                    transition: '1s',
-                    left: '20%',
-                    '&:before': {
-                        content: '""',
-                        position: 'absolute',
-                        height: { xs: '300px', sm: '320px', md: '340px', lg: '360px' },
-                        width: { xs: '100vw', sm: '480px', md: '550px', lg: '680px' },
-                        left: '50%',
-                        transform: 'translateX(-50%)',
-                        borderRadius: '50%',
-                        background: 'radial-gradient(circle, #54126350, #3B126390)',
-                        opacity: 0.1,
-                        filter: 'blur(40px)',
-                        zIndex: -1,
-                        transition: '1s',
-                    },
-                    '&:after': {
-                        content: '""',
-                        position: 'absolute',
-                        zIndex: -20,
-                        height: { xs: '180px', sm: '180px', md: '190px', lg: '200px' },
-                        width: { xs: '100px', sm: '240px', md: '290px', lg: '360px' },
-                        right: '33%',
-                        transform: 'translateX(33%)',
-                        background: 'conic-gradient(from 0deg, #5B1263, #2F1263, #3B1263)',
-                        opacity: 0.4,
-                        filter: 'blur(40px)',
-                        transition: '1s',
-                    },
-                }}
-            ></Box>
+            <SelfImage />
 
             <div className='mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left'>
                 <a
---
+++ src/components/Images/ImageBackdrop.tsx
@@ -0,0 +1,52 @@
+import { Box } from '@mui/material'
+import { type PropsWithChildren } from 'react'
+
+interface ImageBackdropProps {
+    left?: string
+}
+
+const ImageBackdrop = ({ left = '28%', children }: PropsWithChildren<ImageBackdropProps>) => {
+    return (
+        <Box
+            sx={{
+                position: 'relative',
+                display: 'flex',
+                alignItems: 'center',
+                zIndex: -1,
+                transition: '1s',
+                left,
+                '&:before': {
+                    content: '""',
+                    position: 'absolute',
+                    height: { xs: '300px', sm: '320px', md: '340px', lg: '360px' },
+                    width: { xs: '100vw', sm: '480px', md: '550px', lg: '680px' },
+                    left: '50%',
+                    transform: 'translateX(-50%)',
+                    borderRadius: '50%',
+                    background: 'radial-gradient(circle, #54126350, #3B126390)',
+                    opacity: 0.1,
+                    filter: 'blur(40px)',
+                    zIndex: -1,
+                    transition: '1s',
+                },
+                '&:after': {
+                    content: '""',
+                    position: 'absolute',
+                    zIndex: -20,
+                    height: { xs: '180px', sm: '180px', md: '190px', lg: '200px' },
+                    width: { xs: '100px', sm: '240px', md: '290px', lg: '360px' },
+                    right: '33%',
+                    transform: 'translateX(33%)',
+                    background: 'conic-gradient(from 0deg, #5B1263, #2F1263, #3B1263)',
+                    opacity: 0.4,
+                    filter: 'blur(40px)',
+                    transition: '1s',
+                },
+            }}
+        >
+            {children}
+        </Box>
+    )
+}
+
+export default ImageBackdrop
