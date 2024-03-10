5841704a7a9eb09057973636ddb12fe089e169e5
Charlie Davis <34718612+charliecarrot@users.noreply.github.com>
2024-03-09 21:25:44 -0700

    Tailwind config fixes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,15 +3,15 @@ import type { Config } from 'tailwindcss'
 const config: Config = {
     content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
     theme: {
-        colors: {
-            purple: {
-                DEFAULT: '#20172C',
-            },
-        },
         extend: {
             textColor: {
                 primary: '#DEDAE3',
                 secondary: '#756B83',
+            },
+            colors: {
+                purple: {
+                    DEFAULT: '#20172C',
+                },
             },
         },
     },
