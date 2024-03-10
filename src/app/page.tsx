864e6b85323029cf91564270b78f86517fb7e4a5
Charlie Davis <34718612+charliecarrot@users.noreply.github.com>
2024-03-09 23:04:43 -0700

    Add tabler icons and update project links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+'use client'
 import type { NextPage } from 'next'
 import Link from 'next/link'
 import { projects } from './_projects'
+import { IconExternalLink } from '@tabler/icons-react'
 
 const Home: NextPage = () => {
     return (
@@ -31,20 +33,26 @@ const Home: NextPage = () => {
                     className='mb-32 grid h-fit gap-4 text-left transition-all sm:grid-cols-2  md:-ml-2 md:mb-0 md:w-full md:max-w-3xl md:grid-cols-3'
                 >
                     {projects.map((p) => (
-                        <div
+                        <Link
                             key={p.title}
                             id={p.title.replace(' ', '-') + '-link'}
-                            className=' from-violet-700/20 to-fuchsia-600/10 group h-fit rounded-lg px-2 py-3 outline-none transition-all ease-in hover:bg-gradient-to-br md:hover:outline-1'
+                            href={p.href}
+                            target='_blank'
+                            title='Open GitHub Page'
                         >
-                            <Link href={p.href} target='_blank'>
-                                <h3 className='group-hover:text-gray-100 text-sm font-medium tracking-wide underline underline-offset-4 transition-all group-hover:underline-offset-2'>
+                            <div className='from-violet-700/20 to-fuchsia-600/10 group h-fit min-h-24 rounded-lg px-2 py-3 outline-none transition-all ease-in hover:bg-gradient-to-tr md:min-h-28 md:hover:bg-gradient-to-br md:hover:outline-1'>
+                                <h3 className='group-hover:text-gray-100 mr-1 flex justify-between align-middle text-sm font-medium tracking-wide underline underline-offset-4 transition-all group-hover:underline-offset-2'>
                                     {p.title}
+                                    <IconExternalLink
+                                        size={16}
+                                        className='mr-4 mt-0.5 opacity-0 transition-all ease-in group-hover:mr-0 group-hover:opacity-70'
+                                    />
                                 </h3>
                                 <p className='text-gray-100 mt-2 max-w-[40ch] text-sm opacity-80 transition-all group-hover:opacity-90 md:group-hover:mt-3'>
                                     {p.description}
                                 </p>
-                            </Link>
-                        </div>
+                            </div>
+                        </Link>
                     ))}
                 </div>
             </div>
